0290f0c6bc794f2911c48704fe2e44dc596de6aa
Cathy Dao <cathydao95@gmail.com>
2023-09-18 20:54:13 -0700

    create get routes

--- server/server.js
+++ server/server.js
@@ -15,19 +15,40 @@ app.use(express.json());
 
 // //creates an endpoint for the route `/`
 app.get("/", (req, res) => {
-  res.json("Hello ");
+  res.json("Hello Animal tracker");
 });
 
-// app.get("/api/", async (req, res) => {
-//   try {
-//     const { rows: events } = await db.query("SELECT * FROM events");
-//     console.log("lol", events);
-//     res.send(events);
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(400).json({ error });
-//   }
-// });
+app.get("/api/v1/species", async (req, res) => {
+  try {
+    const results = await db.query("SELECT * FROM species");
+    console.log(results);
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: { species: results.rows },
+    });
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.get("/api/v1/species/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await db.query("SELECT * FROM species WHERE species_id=$1", [
+      id,
+    ]);
+    console.log(result);
+    res.status(200).json({
+      status: "success",
+      results: result.rows.length,
+      data: { species: result.rows },
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error });
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Server running on Port http://localhost:${PORT}`)
