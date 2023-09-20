c635e69ac21c989a3dc6d35983f0c7316f382b7b
Cathy Dao <cathydao95@gmail.com>
2023-09-19 23:33:04 -0700

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
+import AllSpecies from "./components/Species/AllSpecies";
+import IndividualSighting from "./components/Sightings/IndividualSighting";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import IndividualSpecies from "./components/Individuals/IndividualSpecies";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div className="title">
+        <Link to="/">Animal Sighting Tracker</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<AllSpecies />}></Route>
+        <Route path="/:id" element={<IndividualSpecies />}></Route>
+        {/* <Route path="/individuals" element={<AllIndividuals />}></Route> */}
+        <Route
+          path="/individuals/:id/sighting"
+          element={<IndividualSighting />}
+        ></Route>
+        {/* <Route path="/sightings" element={<AllSightings />}></Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
